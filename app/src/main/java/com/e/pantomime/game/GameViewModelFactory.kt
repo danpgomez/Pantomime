a9e1c8534cc6ebbb6689d6c77f0e6f58bb19d826
Daniel Perez-Gomez <dpgomez@squarespace.com>
2020-05-11 20:02:47 -0400

    Delete unnecessary log statement

--- app/src/main/java/com/e/pantomime/game/GameViewModelFactory.kt
+++ app/src/main/java/com/e/pantomime/game/GameViewModelFactory.kt
@@ -8,7 +8,6 @@ import java.lang.IllegalArgumentException
 class GameViewModelFactory(private val category: Category) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(GameViewModel::class.java)) {
-            Log.i("GameViewModelFactory", "Category passed is: $category")
             return GameViewModel(category) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
